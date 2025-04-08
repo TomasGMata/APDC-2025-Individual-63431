0e4f56d49009b73d68adea3088b0af49db2ec146
TomasGMata <t.mata@campus.fct.unl.pt>
2025-04-08 23:38:11 +0100

    creation of resources and utils to be used on first REST operation on project

---
+++ src/main/java/pt/apdc/individual63431/util/LoginData.java
@@ -0,0 +1,29 @@
+package pt.apdc.individual63431.util;
+
+public class LoginData {
+
+    public String username;
+    public String email;
+    public String fullName;
+    public Sring phoneNumber;
+    public String password;
+    public String privacy;
+
+    public String ccNumber;
+    public String role;
+    public String NIF;
+    public String company;
+    public String jobTitle;
+    public String address;
+    public String companyNIF;
+    public String state;
+
+    public LoginData() {
+
+    }
+
+    public LoginData(String username, String email, String fullName, String phoneNumber, String password, String privacy) {
+
+    }
+
+}
